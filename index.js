54ab61e3b7a908e8781b36b9dcd5a9ee439e80a9
Cahyadi <cahyaditkj626@gmail.com>
2022-12-07 13:43:05 +0700

    menambahkan model dan migrasi database

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const { Sequelize } = require('sequelize');
 
+// inisialisasi untuk pengamambilan query
+const { QueryTypes } = require('sequelize');
+
+// melakukan koneksi
 const sequelize = new Sequelize('node_orm', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 });
 
 async function main() {
+   // pengecekan test koneksi
    try {
    await sequelize.authenticate();
-   console.log('Connection has been established successfully.');
+      console.log('Connection has been established successfully.');
+      
+      // melakukan query langsung ke database
+      const users = await sequelize.query("SELECT * FROM `users`", { type: QueryTypes.SELECT });
+      // We didn't need to destructure the result here - the results were returned directly
+      console.log(users);
+
+      // kalau error
    } catch (error) {
    console.error('Unable to connect to the database:', error);
       }
